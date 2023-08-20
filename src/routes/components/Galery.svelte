a1590d8df11e4e72576f22955c8482366ec46193
Enzo Pedro Bonacina <enzo.bonacina@softplan.com.br>
2023-08-20 18:28:56 -0300

    Unfinished implementation of the galery.

---
+++ src/routes/components/Galery.svelte
@@ -0,0 +1,42 @@
+<style>
+h3 {
+    background-color: var(--galaxy-dark-blue);
+    color:white;
+    border-radius: var(--default-border-radius);
+    text-align: center;
+    padding-top: 2px;
+    padding-bottom: 2px;
+}
+
+ul {
+    list-style: none;
+    background-color: pink;
+    border-radius: var(--default-border-radius);
+    display: flex;
+    flex-flow: row wrap;
+}
+
+li {
+    width: 33%;
+}
+</style >
+
+<script>
+import Movie from "./Movie.svelte";
+
+function getVideoFiles() {
+    console.log("Getting all available movies");
+    return ["banana", "maçã", "melancia", "laranja", "melão"];
+}
+
+let files = getVideoFiles();
+</script>
+
+<h3>My galery</h3>
+<div>
+    <ul>
+        {#each files as file}
+            <li><Movie /></li>
+        {/each}
+    </ul>
+</div>
---
+++ src/routes/components/Movie.svelte
@@ -0,0 +1,44 @@
+<style>
+button {
+    background-color: var(--galaxy-dark-blue);
+    color: white;
+    border-radius: var(--default-border-radius);
+    border-style: None;
+    padding-top: 3px;
+    padding-bottom: 3px;
+    padding-left: 7px;
+    padding-right: 7px;
+    text-align: center;
+}
+
+div {
+    border: black solid;
+    border-radius: var(--default-border-radius);
+    margin: 10px;
+    padding: 10px;
+}
+
+.title {
+    border-radius: var(--default-border-radius); 
+    background-color: var(--galaxy-dark-blue);
+    color: white;
+    padding-top: 7px;
+    padding-bottom: 7px;
+    padding-left: 15px;
+    padding-right: 15px;
+}
+</style>
+
+<script>
+function handleClick() {
+    console.log("This button is working");
+}
+</script>
+
+<div>
+    <p class="title">A very, very large title, that should not fit in a single movie</p>
+    <p>A very, very large author name, that should not fit in a single movie</p>
+    <p>Duration 1</p>
+    <p>Image 1</p>
+    <button on:click={handleClick}>Play</button>
+</div>
